5a35fd67ebe08047236bff41dda1deccb65d4fb3
Mateo Gonzalez Marulanda <teogonmar97@gmail.com>
2023-03-28 06:36:41 -0500

    add context and messages

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import openai #Fue instalada anteriormente
 import config
 
+#config:
+#api_key = "api key from https://platform.openai.com/account/api-keys"
+
 openai.api_key = config.api_key
 
+#Contexto del asistente: https://platform.openai.com/docs/guides/chat/introduction
+messages = [{"role":"system",
+        "content": "Eres un asistente muy util"}] #Se puede acotar mas el contexto
+
+content = input('Sobre que quieres hablar?')
+
+#Hacemos que los mensajes sean constantes y se agregue uno tras otro
+messages.append({"role":"user", "content": content})
+
 #Basandonos en la documentacion: https://platform.openai.com/docs/models/gpt-3-5
+#Esta es la peticion que se le hace a Chat GPT para que responda. La pregunta la enviaremos desde content.
 response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                            messages = [{"role":"user", "content":"Cual es la mision de OpenAI?"}]) #
+                            messages = messages)
 
 print(response.choices[0].message.content)
